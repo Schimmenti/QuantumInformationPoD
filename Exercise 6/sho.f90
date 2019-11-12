b5b5b57962cfacf414209b60a2a242cd1d9a0114
Schimmenti <vinci95s@gmail.com>
2019-11-12 14:45:04 +0100

    update

--- Exercise 6/sho.f90
+++ Exercise 6/sho.f90
@@ -3,7 +3,7 @@ implicit none
 contains
 
 ! second derivative and exact up to eps^2
-function dx2_2(psi,eps,N)result(phi)
+function dx2_2(psi,eps,N, pbc=.TRUE.)result(phi)
 ! lattice size
 	real*4 :: eps
 ! number of discretized points and loop index
@@ -13,21 +13,57 @@ function dx2_2(psi,eps,N)result(phi)
 	complex*8, dimension(:), allocatable :: phi
 
 	allocate(phi(N))
-! assume psi(-1)=0
-! assume psi(N+1)=0
-	phi(1)=(psi(2)-2*psi(1))/(eps*eps)
-	phi(N)=(-2*psi(N)+psi(N-1))/(eps*eps)
+
+	if(pbc)then
+		phi(1)=(psi(2)-2*psi(1)+psi(N))/(eps*eps)
+		phi(N)=(psi(1)-2*psi(N)+psi(N-1))/(eps*eps)
+	else
+		phi(1)=(psi(2)-2*psi(1))/(eps*eps)
+		phi(N)=(-2*psi(N)+psi(N-1))/(eps*eps)
+	end if
 	do ii=2,N-1
 		phi(ii)=(psi(ii+1)-2*psi(ii)+psi(ii-1))/(eps*eps)
 	end do
 	
 end function
 
-function hoscx_2(psi, V,eps, N, omega=sqrt(2),hbar=1.0,m=0.5)result(phi)
+function laplmatr(eps, N, pbc=.TRUE.)result(A)
 ! lattice size
 	real*4 :: eps
 ! number of discretized points and loop index
 	integer*4 :: N,ii
+
+	real*4, dimension(:,:), allocatable :: A
+	allocate(A(N,N))
+	A=0
+	if(pbc)
+		A(1,1)=-2
+		A(1,2)=1
+		A(1,N)=1
+
+		A(N,N)=-2
+		A(N,N-1)=1
+		A(N,1)=1	
+	else
+		A(1,1)=-2
+		A(1,2)=1
+
+		A(N,N)=-2
+		A(N,N-1)=1
+	end if
+	do ii=2,N-1
+		A(ii,ii)=-2
+		A(ii,ii+1)=1
+		A(ii,ii-1)=1
+	end do
+	
+end function
+
+function hoscx_2(psi,eps, L, omega=sqrt(2),hbar=1.0,m=0.5)result(phi)
+! lattice size
+	real*4 :: eps
+! number of discretized points and loop index
+	integer*4 :: N,L,ii
 ! mass
 	real*4 :: m
 ! hbar
@@ -37,13 +73,15 @@ function hoscx_2(psi, V,eps, N, omega=sqrt(2),hbar=1.0,m=0.5)result(phi)
 ! wave function
 	complex*8, dimension(:) :: psi
 	complex*8, dimension(:), allocatable :: phi
-
+	N=2*L+1
 	phi=dx2_2(psi, eps, N)
 	phi = ((-hbar*hbar)/(2*m))*phi
-	do ii=1,N
-		phi(ii)=V(ii*eps)*psi(ii)
+	do ii=-L,L
+		phi(ii)=phi(ii)+(0.5*omega*omega)*((ii*eps)**2)*psi(ii+L+1)
 	end do
 end function
+
+
 
 
 
